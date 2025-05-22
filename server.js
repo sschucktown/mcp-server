1ea62861f72a1a5215ad13372a9ccec6b91e718f
Scott <scottmsandie@gmail.com>
2025-05-22 11:17:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ const checkConditions = require('./routes/checkConditions');
 const addEvent = require('./routes/addEvent');
 const freeBlocks = require('./routes/freeBlocks');
 const checkAndSuggest = require('./routes/checkAndSuggest');
+const fitSummary = require('./routes/fitSummary');
 
 // Routes
 app.use('/check-kayak-conditions', checkConditions);
 app.use('/add-kayak-event', addEvent);
 app.use('/free-blocks', freeBlocks);
 app.use('/check-and-suggest', checkAndSuggest);
+app.use('/fit-summary', fitSummary);
 
 app.get('/', (req, res) => {
     res.send('MCP Kayaking Server is Live 🚣');
